c4d96adb4d810eae8f4692fa7730403c27c8d613
Beloc <thierry@beobject.be>
2021-01-11 01:30:57 +0100

    Close modal fixes #5

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// Menu burger
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -11,11 +12,23 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalCloseBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// stop modal event
+modalCloseBtn.addEventListener("click", stopModal);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+// close modal form
+function stopModal() {
+  modalbg.style.display = "none";
+}
+
+// display log
+console.log(modalBtn);
