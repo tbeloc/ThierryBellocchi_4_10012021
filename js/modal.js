c3a62a2b17908411ba703596b233439fe81e804d
Beloc <thierry@beobject.be>
2021-01-24 20:42:28 +0100

    After mod function JS

--- js/modal.js
+++ js/modal.js
@@ -33,35 +33,55 @@ function stopModal() {
 modalCloseBtn.addEventListener("click", stopModal);
 
 //
-function checkInput(input, type, pattern) {
-  console.log(input);
+function checkInput(input, type, pattern, min, max) {
+  // console.log(input);
   const firstInput = document.getElementById(input);
+
   if (pattern != null) {
     firstInput.setAttribute("pattern", pattern);
   }
+
+  if (min != null) {
+    firstInput.setAttribute("min", min);
+  }
+
+  if (max != null) {
+    firstInput.setAttribute("max", max);
+  }
+
   firstInput.setAttribute("required", "");
   firstInput.setAttribute("type", type);
   firstInput.addEventListener("blur", goCheck);
 
   function goCheck() {
     const error = document.getElementById(input + "-error");
-    if (!firstInput.validity.valid) {
-      // console.log("NOK");
-      // console.log(firstInput);
-      error.style.opacity = 1;
-    } else {
+    //checkValidity
+    if (firstInput.checkValidity()) {
+      // if (!firstInput.validity.valid) {
       error.style.opacity = 0;
+    } else {
+      error.style.opacity = 1;
     }
   }
 }
 
-function checkDate(input) {}
+//checkValidity
 
 // Validation du nom
-checkInput("first", "text", "[A-Za-z]{2,}");
+checkInput("first", "text", "[A-Za-z]{2,}", null, null);
 
 // Validation du prénom
-checkInput("last", "text", "[A-Za-z]{2,}");
+checkInput("last", "text", "[A-Za-z]{2,}", null, null);
 
 // Validation email
-checkInput("email", "email", null);
+checkInput("email", "email", null, null, null);
+
+//
+checkInput("quantity", "number", null, 0, 99);
+
+var checkboxes = document.querySelectorAll('input[type="radio"]:checked');
+
+// check lenght
+
+var checkedOne = Array.prototype.slice.call(checkboxes).some((x) => x.checked);
+console.log(checkedOne);
