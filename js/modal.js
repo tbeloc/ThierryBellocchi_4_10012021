b185d2c8f1e6a88ffdced24dbe0a3cdc9dd0cbbe
Beloc <thierry@beobject.be>
2021-01-30 18:20:27 +0100

    Add dart-sass, webpack, babel-loader

--- js/modal.js
+++ js/modal.js
@@ -1,91 +1,32 @@
-// menu burger
+// dom
+const modal = document.querySelector(".bground");
+const modalOpenBtn = document.querySelectorAll(".modal-btn");
+const modalCloseBtn = document.querySelector(".close");
+
+// menu responsive
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+  var nav = document.getElementById("myTopnav");
+  if (nav.className === "topnav") {
+    nav.className += " responsive";
   } else {
-    x.className = "topnav";
+    nav.className = "topnav";
   }
 }
 
-// dom elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalCloseBtn = document.querySelector(".close");
-const checkboxConditions = document.getElementById("checkbox1");
-// const formSubscriber = document.getElementById("formSubscriber");
-
-// function close modal form
-function stopModal() {
-  modalbg.style.display = "none";
-}
-
-// function launch modal form
+// open modal
 function launchModal() {
-  modalbg.style.display = "block";
+  modal.style.display = "block";
+  // validSubmit();
   document.getElementById("first").focus();
 }
 
-// start modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal
+function stopModal() {
+  modal.style.display = "none";
+}
 
-// stop modal event
+// open modal
+modalOpenBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal
 modalCloseBtn.addEventListener("click", stopModal);
-
-// form conditions
-checkboxConditions.addEventListener("change", validSubmit);
-
-//
-function checkInput(input) {
-  const inputElement = document.getElementById(input);
-  function goCheck() {
-    const error = document.getElementById(input + "-error");
-    if (inputElement.checkValidity()) {
-      console.log(inputElement);
-      error.style.opacity = 0;
-      inputElement.style.borderColor = "#ffffff";
-    } else {
-      console.log(inputElement);
-      error.style.opacity = 1;
-      inputElement.style.borderColor = "#e54858";
-    }
-    validSubmit();
-  }
-  inputElement.addEventListener("blur", goCheck);
-}
-
-function validSubmit() {
-  var isValid = true;
-  const inputAll = document.querySelectorAll("input");
-
-  inputAll.forEach((element) => validForm(element));
-
-  function validForm(element) {
-    if (!element.checkValidity()) {
-      isValid = false;
-    }
-  }
-
-  if (isValid) {
-    document.getElementById("btn-submit").setAttribute("disabled", "false");
-    document.getElementById("btn-submit").style.opacity = "1";
-    console.log(document.getElementById("btn-submit"));
-  } else {
-    document.getElementById("btn-submit").setAttribute("disabled", "true");
-    document.getElementById("btn-submit").style.opacity = "0.2";
-    console.log(document.getElementById("btn-submit"));
-  }
-}
-
-// Validation du nom
-checkInput("first");
-
-// Validation du prénom
-checkInput("last");
-
-// Validation email
-checkInput("email");
-
-//
-checkInput("quantity");
---
+++ js/validity.js
@@ -0,0 +1,49 @@
+//
+const checkboxConditions = document.getElementById("checkbox1");
+checkboxConditions.addEventListener("change", validSubmit);
+
+//
+function validSubmit() {
+  var isValid = true;
+  const inputAll = document.querySelectorAll("input");
+  const btnSubmit = document.getElementById("btn-submit");
+
+  inputAll.forEach((element) => validForm(element));
+
+  function validForm(element) {
+    if (!element.checkValidity()) {
+      isValid = false;
+    }
+  }
+
+  if (isValid) {
+    btnSubmit.setAttribute("disabled", "false");
+    btnSubmit.style.opacity = "1";
+  } else {
+    btnSubmit.setAttribute("disabled", "true");
+    btnSubmit.style.opacity = "0.2";
+  }
+}
+
+//
+function checkInput(input) {
+  const inputElement = document.getElementById(input);
+  function goCheck() {
+    const error = document.getElementById(input + "-error");
+    if (inputElement.checkValidity()) {
+      error.style.opacity = 0;
+      inputElement.style.borderColor = "#ffffff";
+    } else {
+      error.style.opacity = 1;
+      inputElement.style.borderColor = "#e54858";
+    }
+    validSubmit();
+  }
+  inputElement.addEventListener("blur", goCheck);
+}
+
+//
+checkInput("first");
+checkInput("last");
+checkInput("email");
+checkInput("quantity");
